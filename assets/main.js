ff52d9b3f418f7bf093c2c6c44d607ee6e43e032
Topi Särkiniemi <topi.sarkiniemi@gmail.com>
2020-04-16 18:00:31 +0300

    Images and animations

--- assets/main.js
+++ assets/main.js
@@ -17,23 +17,52 @@
 })();
 
 // Scroll elements
-const sections = document.getElementsByClassName("scroll-appear");
-const goal = 150;
+
 const windowHeight = window.innerHeight;
+const appears = document.getElementsByClassName("scroll-appear");
+const movers = document.getElementsByClassName("scroll-move");
+const goal = 200;
+const goal2 = 150;
+const spinners = document.getElementsByClassName("scroll-spin");
+const slowspinners = document.getElementsByClassName("slower-spin");
 
 // Scroll settings
 window.addEventListener("optimizedScroll", function(){
-    for (let i = 0; i < sections.length; i++) {
-        let fromViewportTop = sections[i].getBoundingClientRect().top;
+    for (let i = 0; i < appears.length; i++) {
+        let fromViewportTop = appears[i].getBoundingClientRect().top;
         if (fromViewportTop < windowHeight && fromViewportTop >= windowHeight / 2) {
-            sections[i].style.opacity = "" + 2 - ( 2 * fromViewportTop / windowHeight ) + "";
-            sections[i].style.transform = "matrix(1,0,0,1,0," + 2 * goal * ( (fromViewportTop - windowHeight / 2) / windowHeight ) + ")";
+            appears[i].style.opacity = "" + 2 - ( 2 * fromViewportTop / windowHeight ) + "";
+            appears[i].style.transform = "matrix(1,0,0,1,0," + 2 * goal * ( (fromViewportTop - windowHeight / 2) / windowHeight ) + ")";
         } else if (fromViewportTop < windowHeight / 2) {
-            sections[i].style.opacity = "1";
-            sections[i].style.transform = "matrix(1,0,0,1,0,0)";
+            appears[i].style.opacity = "1";
+            appears[i].style.transform = "matrix(1,0,0,1,0,0)";
         } else {
-            sections[i].style.opacity = "0";
-            sections[i].style.transform = "matrix(1,0,0,1,0,150)";
+            appears[i].style.opacity = "0";
+            appears[i].style.transform = "matrix(1,0,0,1,0," + goal + ")";
+        }
+    }
+    for (let i = 0; i < spinners.length; i++) {
+        let fromViewportTop = spinners[i].getBoundingClientRect().top;
+        let elementHeight = spinners[i].getBoundingClientRect().height;
+        if (fromViewportTop >= - elementHeight) {
+            spinners[i].style.transform = "rotate(" + window.pageYOffset + "deg)";
+        }
+    }
+    for (let i = 0; i < slowspinners.length; i++) {
+        let fromViewportTop = slowspinners[i].getBoundingClientRect().top;
+        let elementHeight = slowspinners[i].getBoundingClientRect().height;
+        if (fromViewportTop >= - elementHeight && fromViewportTop < windowHeight) {
+            slowspinners[i].style.transform = "rotate(" + window.pageYOffset / 4 + "deg)";
+        }
+    }
+    for (let i = 0; i < movers.length; i++) {
+        let fromViewportTop = movers[i].getBoundingClientRect().top;
+        if (fromViewportTop < windowHeight && fromViewportTop >= windowHeight / 2) {
+            movers[i].style.transform = "matrix(1,0,0,1,0," + 2 * goal2 * ( (fromViewportTop - windowHeight / 2) / windowHeight ) + ")";
+        } else if (fromViewportTop < windowHeight / 2) {
+            movers[i].style.transform = "matrix(1,0,0,1,0,0)";
+        } else {
+            movers[i].style.transform = "matrix(1,0,0,1,0,"+ goal2 + ")";
         }
     }
 });
