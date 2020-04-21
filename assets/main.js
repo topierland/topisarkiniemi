0a5bb81ddd52176e2133d14301949e924d34caa8
Topi Särkiniemi <topi.sarkiniemi@gmail.com>
2020-04-21 11:13:50 +0300

    Improved animation performance

--- assets/main.js
+++ assets/main.js
@@ -33,12 +33,15 @@ window.addEventListener("optimizedScroll", function(){
         if (fromViewportTop < windowHeight && fromViewportTop >= windowHeight / 2) {
             appears[i].style.opacity = "" + 2 - ( 2 * fromViewportTop / windowHeight ) + "";
             appears[i].style.transform = "matrix(1,0,0,1,0," + 2 * goal * ( (fromViewportTop - windowHeight / 2) / windowHeight ) + ")";
-        } else if (fromViewportTop < windowHeight / 2) {
+            appears[i].setAttribute("animate", "true");
+        } else if (fromViewportTop < windowHeight / 2 && appears[i].getAttribute("animate") === "true") {
             appears[i].style.opacity = "1";
             appears[i].style.transform = "matrix(1,0,0,1,0,0)";
-        } else {
+            appears[i].setAttribute("animate", "false");
+        } else if (appears[i].getAttribute("animate") === "true") {
             appears[i].style.opacity = "0";
             appears[i].style.transform = "matrix(1,0,0,1,0," + goal + ")";
+            appears[i].setAttribute("animate", "false");
         }
     }
     for (let i = 0; i < spinners.length; i++) {
@@ -52,17 +55,20 @@ window.addEventListener("optimizedScroll", function(){
         let fromViewportTop = slowspinners[i].getBoundingClientRect().top;
         let elementHeight = slowspinners[i].getBoundingClientRect().height;
         if (fromViewportTop >= - elementHeight && fromViewportTop < windowHeight) {
-            slowspinners[i].style.transform = "rotate(" + window.pageYOffset / 4 + "deg)";
+            slowspinners[i].style.transform = "rotate(" + Math.round( window.pageYOffset / 4 ) + "deg)";
         }
     }
     for (let i = 0; i < movers.length; i++) {
         let fromViewportTop = movers[i].getBoundingClientRect().top;
         if (fromViewportTop < windowHeight && fromViewportTop >= windowHeight / 2) {
             movers[i].style.transform = "matrix(1,0,0,1,0," + 2 * goal2 * ( (fromViewportTop - windowHeight / 2) / windowHeight ) + ")";
-        } else if (fromViewportTop < windowHeight / 2) {
+            movers[i].setAttribute("animate", "true");
+        } else if (fromViewportTop < windowHeight / 2 && movers[i].getAttribute("animate") === "true") {
             movers[i].style.transform = "matrix(1,0,0,1,0,0)";
-        } else {
+            movers[i].setAttribute("animate", "false");
+        } else if (movers[i].getAttribute("animate") === "true") {
             movers[i].style.transform = "matrix(1,0,0,1,0,"+ goal2 + ")";
+            movers[i].setAttribute("animate", "false");
         }
     }
 });
